9f4db3675ee03d6d7ac46dcf5a252a68d2daa9d7
Srinath Sridharan <srinathsr@google.com>
2016-09-08 13:47:02 -0700

    ANDROID: sched/rt: rt cpu selection integration with EAS.
    joshuous:
    * Adapted for kernel/common
    * "ANDORID: sched/rt: fix schedtune accouting on prio change" not needed
    
    For effective interplay between RT and fair tasks. Enables sched_fifo
    for UI and Render tasks. Critical for improving user experience.
    
    bug: 24503801
    bug: 30377696
    Change-Id: I2a210c567c3f5c7edbdd7674244822f848e6d0cf
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    (cherry picked from commit dfe0f16b6fd3a694173c5c62cf825643eef184a3)
    Signed-off-by: joshuous <joshuous@gmail.com>
    
    Conflicts:
    	kernel/sched/rt.c

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1629,17 +1629,15 @@ static inline unsigned long task_util(struct task_struct *p)
 		return (demand << 10) / walt_ravg_window;
 	}
 #endif
-	return READ_ONCE(p->se.avg.util_avg);
+	return p->se.avg.util_avg;
 }
 
 
-/**
- * Amount of capacity of a CPU that is (estimated to be) used by CFS tasks
- * @cpu: the CPU to get the utilization of
- *
- * The unit of the return value must be the one of capacity so we can compare
- * the utilization with the capacity of the CPU that is available for CFS task
- * (ie cpu_capacity).
+/*
+ * cpu_util returns the amount of capacity of a CPU that is used by CFS
+ * tasks. The unit of the return value must be the one of capacity so we can
+ * compare the utilization with the capacity of the CPU that is available for
+ * CFS task (ie cpu_capacity).
  *
  * cfs_rq.avg.util_avg is the sum of running time of runnable tasks plus the
  * recent utilization of currently non-runnable tasks on a CPU. It represents
